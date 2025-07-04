87e895211fe9e3298506bcec9eb0a3dfbf625253
mikha <shavrin.mikhail855@gmail.com>
2025-07-04 08:07:34 +0300

    Добавил Before анатоцию для проверки работоспособности пропертей

--- src/test/java/com/base/BaseTest.java
+++ src/test/java/com/base/BaseTest.java
@@ -2,11 +2,15 @@ package com.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.utils.TestConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.ui.tests.LoginTestBase.logger;
 
 public class BaseTest {
 
@@ -33,6 +37,16 @@ public class BaseTest {
         Configuration.browserCapabilities = options;
         Configuration.browserSize = null; // потому что уже стартуем в максимальном размере
         Configuration.baseUrl = "https://www.saucedemo.com";
+    }
+
+    @BeforeAll
+    static void validateTestConfig() {
+        String login = TestConfig.getLogin("standard_user");
+        String password = TestConfig.getPassword("standard_user");
+        if (login.isEmpty() || password.isEmpty()) {
+            throw new IllegalStateException("Конфигурация логина/пароля для standard_user не задана или пуста");
+        }
+        logger.info("Конфиг прошёл проверку. Логин: {}, пароль: <скрыт>", login);
     }
 
     @AfterEach
