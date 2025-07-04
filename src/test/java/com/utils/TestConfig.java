5b98f8583afe4f56d1fbc5ba4a6e757c16769c94
mikha <shavrin.mikhail855@gmail.com>
2025-07-04 08:06:47 +0300

    Реализовал хранение данных в пропертях и реализовал класс, через который можно работать с пропертями

---
+++ src/test/java/com/utils/TestConfig.java
@@ -0,0 +1,37 @@
+package com.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TestConfig {
+
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = TestConfig.class.getClassLoader().getResourceAsStream("test.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Файл test.properties не найден в ресурсах");
+            }
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка загрузки test.properties", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return props.getProperty(key);
+    }
+
+    public static String getLogin(String userKey) {
+        return props.getProperty("login." + userKey + ".username", "");
+    }
+
+    public static String getPassword(String userKey) {
+        return props.getProperty("login." + userKey + ".password", "");
+    }
+
+    public static String getBaseUrl() {
+        return props.getProperty("url.base");
+    }
+}
